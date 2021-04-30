d695a43ce7786c7b7e7525c83b11ffd39ba392d1
Shreedhar73 <shreedharpandey000@gmail.com>
2021-04-30 13:31:12 +0545

    detailsstarted

--- lib/Pages/Details.dart
+++ lib/Pages/Details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hotel_booking/global.dart';
+import 'package:hotel_booking/Pages/Home.dart';
 
 
 class DetailsPage extends StatelessWidget {
@@ -7,6 +8,47 @@ class DetailsPage extends StatelessWidget {
   const DetailsPage({Key key, this.id}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.pink.shade50,
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+                child: Column(
+                  children: [
+                    Expanded(child: Stack(
+                      children: [
+                        Positioned(
+                          top: 0,
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: Image.asset(hotelsList[id].imagePath,
+                          fit: BoxFit.cover,),
+
+                        ),
+
+                        Positioned(
+                          
+                          left: 6,
+                          right: 6,
+                          child: IconButton(
+                            icon: Icon(Icons.chevron_left_outlined,
+                            color: Colors.white,),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                          
+                          
+                        )
+                      ],
+                    ))
+                  ],
+                )
+            )
+          ],
+        ),
+      ),
+
+    );
   }
 }
--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hotel_booking/Pages/Details.dart';
 import 'package:hotel_booking/global.dart';
@@ -89,36 +90,41 @@ _buildAppBar(BuildContext context) {
                           builder: (context) => DetailsPage(id : i),
                       )
                       ),
-                      child: Container(
-                        width: 150,
-                        margin: const EdgeInsets.symmetric(horizontal: 11.0),
-                        child: Stack(
-                          children: <Widget>[
-                            Positioned.fill(
-                              child: Image.asset(hotelsList[i].imagePath,
-                              fit: BoxFit.cover,) ,
+                      child: Card(
+                        color: Colors.white70,
+                        elevation: 20.0,
 
-                            ),
-                            Positioned(
-                              bottom: 0,
-                              left: 0,
-                              right: 0,
-                              child: Container(
-                                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.white54,
-                                  borderRadius: BorderRadius.only(topRight: Radius.circular(15))
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(hotelsList[i].hotelName),
-                                    Text(hotelsList[i].cityName),
-                                  ],
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.7,
+                          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Stack(
+                            children: <Widget>[
+                              Positioned.fill(
+                                child: Image.asset(hotelsList[i].imagePath,
+                                fit: BoxFit.cover,) ,
+
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white54,
+                                    borderRadius: BorderRadius.only(topRight: Radius.circular(15))
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(hotelsList[i].hotelName),
+                                      Text(hotelsList[i].cityName),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
 
